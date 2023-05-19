dc91ff21718a9e7387544b1f037153f6ee190696
Chalon Lubin <chalonlubin1@gmail.com>
2023-05-18 23:06:34 -0700

    Add accordian and carousel

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./carousel.css"
+
+const images = [
+  "https://images.pexels.com/photos/3836292/pexels-photo-3836292.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/2792157/pexels-photo-2792157.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1166209/pexels-photo-1166209.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+];
+
+function Carousel() {
+  const [current, setCurrent] = useState(0);
+
+  function next() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  function prev() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  return (
+    <><h3> Custom Carousel </h3>
+    <div className="carousel-container">
+
+      <div className="slider">
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className="slide">
+                <div className="slide-content">
+                  <div className="left-arrow" onClick={prev}>
+                    &#x1F448;
+                  </div>
+                  <div className="right-arrow" onClick={next}>
+                    &#x1F449;
+                  </div>
+                  <img src={image} alt="images" />
+                </div>
+              </div>
+            )
+        )}
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Carousel;
